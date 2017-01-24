298c16f20cd48da37c88d8959791157f103e8b05
richmcateer <rmcateer1@gmail.com>
2017-01-23 20:43:18 -0500

    Initial commit

---
+++ Ironc.ly/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Ironc.ly
+//
+//  Created by Richard McAteer on 1/23/17.
+//  Copyright © 2017 Richard McAteer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController: UIViewController = UIViewController()
+        viewController.view.backgroundColor = UIColor.magenta
+        
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
