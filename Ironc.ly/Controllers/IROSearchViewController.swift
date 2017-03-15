3c577901f5b2dc0a8ec570cd7a4d5fc3644de711
richmcateer <rmcateer1@gmail.com>
2017-03-15 07:26:45 -0500

    Tab bar icon

--- Ironc.ly/Controllers/IROSearchViewController.swift
+++ Ironc.ly/Controllers/IROSearchViewController.swift
@@ -10,19 +10,80 @@ import UIKit
 
 class IROSearchViewController: UIViewController {
 
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.red
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.addSubview(self.searchCollectionView)
+        
+        self.setUpConstraints()
     }
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.navigationItem.title = "Private"
+        self.tabBarController?.navigationItem.leftBarButtonItem = nil
+        self.tabBarController?.navigationItem.rightBarButtonItem = nil
+    }
 
+    // MARK: - Lazy Initialization
+    lazy var searchCollectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    // MARK: - Autolayout
+    func setUpConstraints() {
+        self.searchCollectionView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.searchCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.searchCollectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.searchCollectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+    }
+
+}
+
+extension IROSearchViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+}
+
+extension IROSearchViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (collectionView.bounds.width - 4 * 15.0) / 2.0
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0.0, left: 15.0, bottom: 0.0, right: 15.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 30.0
+    }
+    
 }
--- Ironc.ly/Controllers/IROTabBarController.swift
+++ Ironc.ly/Controllers/IROTabBarController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class IROTabBarController: UITabBarController {
     
-    let homeImage: UIImage = UIImage(cgImage: UIImage(named: "home")!.cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
-    let searchImage: UIImage = UIImage(cgImage: UIImage(named: "search")!.cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
-    let cameraImage: UIImage = UIImage(cgImage: UIImage(named: "camera")!.cgImage!, scale: 4.0, orientation: .up).withRenderingMode(.alwaysOriginal)
-    let messageImage: UIImage = UIImage(cgImage: UIImage(named: "message")!.cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
-    let profileImage: UIImage = UIImage(cgImage: UIImage(named: "profile")!.cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
+    let homeImage: UIImage = UIImage(cgImage: #imageLiteral(resourceName: "home").cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
+    let privateImage: UIImage = UIImage(cgImage: #imageLiteral(resourceName: "private").cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
+    let cameraImage: UIImage = UIImage(cgImage: #imageLiteral(resourceName: "camera").cgImage!, scale: 4.0, orientation: .up).withRenderingMode(.alwaysOriginal)
+    let messageImage: UIImage = UIImage(cgImage: #imageLiteral(resourceName: "message").cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
+    let profileImage: UIImage = UIImage(cgImage: #imageLiteral(resourceName: "profile").cgImage!, scale: 3.0, orientation: .up).withRenderingMode(.alwaysOriginal)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +27,7 @@ class IROTabBarController: UITabBarController {
         homeViewController.tabBarItem.tag = 0
         
         let searchViewController: IROSearchViewController = IROSearchViewController()
-        searchViewController.tabBarItem = UITabBarItem(title: nil, image: self.searchImage, selectedImage: nil)
+        searchViewController.tabBarItem = UITabBarItem(title: nil, image: self.privateImage, selectedImage: nil)
         searchViewController.tabBarItem.imageInsets = UIEdgeInsets(top: 5.0, left: 0.0, bottom: -5.0, right: 0.0)
         searchViewController.tabBarItem.tag = 1
 
