8c3fbc471bb64907d7721460ed53e0438496c77c
richmcateer <rmcateer1@gmail.com>
2017-04-06 13:04:49 -0400

    Track Alamofire

---
+++ Pods/Target Support Files/Pods-Ironc.ly/Pods-Ironc.ly-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ironc_ly : NSObject
+@end
+@implementation PodsDummy_Pods_Ironc_ly
+@end
---
+++ Pods/Target Support Files/Pods-Ironc.ly/Pods-Ironc.ly-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Ironc_lyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Ironc_lyVersionString[];
+
