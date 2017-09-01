fba0af876dc9cf14f3ba1a48accbf7cdbf99437e
Alex Laptop <savittalex@gmail.com>
2017-09-01 11:22:55 -0400

    added main page file

---
+++ Ironc.ly/UI/View Controllers/TIPMainPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TIPMainPageViewController.swift
+//  Ironc.ly
+//
+//  Created by Alex Laptop on 9/1/17.
+//  Copyright © 2017 Richard McAteer. All rights reserved.
+//
+
+import UIKit
+
+class TIPMainPageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
